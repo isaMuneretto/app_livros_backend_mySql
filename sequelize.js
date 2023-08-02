2f0b4e9d26c7b850be801f575e63f550d691a47a
isaMuneretto <isamuneretto@gmail.com>
2023-08-02 16:41:06 -0300

    colocado as credenciais com o .env

--- sequelize.js
+++ sequelize.js
@@ -1,9 +1,16 @@
 const { Sequelize } = require('sequelize');
+const dotenv = require('dotenv');
+dotenv.config();
 
-const db = new Sequelize('gestao_livros', 'rhaisa', '27819814', {
-  host: 'localhost',
-  dialect: 'mysql',
-});
+const db = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+      host: process.env.DB_HOST,
+      dialect: "mysql",
+  }
+);
 
 // Verifica se há algum erro na conexão
 db.authenticate()
